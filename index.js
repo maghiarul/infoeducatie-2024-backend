dc8bda44474dea9d6ad60b1b9c3cdf97abb00700
Panaite Cristian <panaite.cristian99@gmail.com>
2024-03-24 14:41:04 +0200

    lol

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM news", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
